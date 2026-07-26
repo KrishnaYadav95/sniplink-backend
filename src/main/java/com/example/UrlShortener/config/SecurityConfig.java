c089919ab2277f6df44e1096f1e649f5f451638f
krishnaaaahh <krishnayadav0816@gmail.com>
2026-07-27 01:10:52 +0530

    pushed thrice

--- src/main/java/com/example/UrlShortener/config/SecurityConfig.java
+++ src/main/java/com/example/UrlShortener/config/SecurityConfig.java
@@ -7,25 +7,44 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http , DaoAuthenticationProvider authenticationProvider) throws Exception{
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, DaoAuthenticationProvider authenticationProvider) throws Exception {
         http
-                .csrf(csrf-> csrf.disable())
-                .authorizeHttpRequests(auth-> auth
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/user/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .httpBasic(Customizer.withDefaults())
                 .formLogin(Customizer.withDefaults())
-                .oauth2Login(oauth2-> oauth2
-                        .defaultSuccessUrl("/url/**" ,true)
+                .oauth2Login(oauth2 -> oauth2
+                        .defaultSuccessUrl("/url/**", true)
                 )
                 .authenticationProvider(authenticationProvider);
-                return http.build();
+        return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("https://sniplink-frontend1.vercel.app"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
