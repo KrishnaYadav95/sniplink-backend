1af44016de2c82ab779411bb59a28ca8052aaffb
krishnaaaahh <krishnayadav0816@gmail.com>
2026-07-27 02:00:53 +0530

    pushhhhh

--- src/main/java/com/example/UrlShortener/config/SecurityConfig.java
+++ src/main/java/com/example/UrlShortener/config/SecurityConfig.java
@@ -39,7 +39,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("https://sniplink-frontend1.vercel.app"));
+
+        configuration.setAllowedOrigins(List.of(
+                "https://sniplink-frontend1.vercel.app"
+        ));
+
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
